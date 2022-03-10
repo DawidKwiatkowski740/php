da9a9e24ca1552bf3d7aa6b73188bb558a140db4
DawidKwiatkowski740 <79514899+DawidKwiatkowski740@users.noreply.github.com>
2022-03-10 15:16:36 +0100

    Add files via upload

---
+++ dawid kwiat/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>kkk</title>
+<meta charset="utf-8" />
+<style
+type="text/css"> @import "index.css" ;
+</style>
+</head>
+
+<body>
+<div id="php">
+
+<form action="wynik.php" method="post">
+imie: <input type="text" name="imie" /><br>
+Hasło: <input type="text" name="nazwisko" /><br>
+
+<button type="submit" value="Button">Submit</button>
+
+</form>
+
+</div>
+<div id="srodek">
+</div>
+</body>
+</html>
